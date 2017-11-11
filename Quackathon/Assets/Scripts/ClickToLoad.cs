136229bfff847f193c42f3c249189c360aeb9fbe
dbrereton1995 <dbrereton1995@gmail.com>
2017-11-11 23:56:27 +0000

    Added Main Menu and Instruction Scenes/Functionality. Added Quit functionality. It's late so please don't judge my code.

---
+++ Quackathon/Assets/Scripts/ClickToLoad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickToLoad : MonoBehaviour {
+
+    public void Load(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
