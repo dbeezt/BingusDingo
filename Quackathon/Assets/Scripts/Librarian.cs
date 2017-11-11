2083f565ea6c0b8153444581f98429a1ac1e34e6
jmsnll <jneill542@gmail.com>
2017-11-11 15:00:38 +0000

    Added A* Pathfinding library

---
+++ Quackathon/Assets/Scripts/Librarian.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Librarian : MonoBehaviour {
+
+    public Vector2 velocity;
+    public Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 current = body.position;
+        Vector2 target = new Vector2(5, 5);
+
+        body.position = Vector2.MoveTowards(current,target,0.1f);
+    }
+
+}
