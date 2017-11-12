5b1ee698471e74121e29e81d72a299584de8591e
TwoRice <2rimagowan@gmail.com>
2017-11-12 09:59:00 +0000

    not sure

---
+++ Quackathon/Assets/Scripts/timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+
+    public float time = 0f;
+
+    private Text timerText;
+
+    void Awake()
+    {
+        timerText = GetComponent<Text>();    
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        time += Time.deltaTime;
+        timerText.text = time.ToString("00.00");
+    }
+}
